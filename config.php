fe577d6b7a841f78e1698002af1699059e302527
BRANSTETT Tim <t.branstett@mairie-stavold.int>
2020-07-12 21:55:56 +0200

    First

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$db_host = "localhost";
+$db_name = "";
+$db_user = "root";
+$db_pass = "";
+
+try{
+    $bdd = new PDO("mysql:host={$db_host};dbname={$db_name}",$db_user,$db_pass);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+    echo $e->getMessage();
+}
+
+?>
