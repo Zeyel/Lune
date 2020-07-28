66d52c14ca792dd005a0ea5014466f1a3484672f
BRANSTETT Tim <t.branstett@mairie-stavold.int>
2020-07-28 14:37:58 +0200

    Gestion de fichiers qui ne répondent pas à la syntaxe

--- menu.php
+++ menu.php
@@ -3,8 +3,10 @@
 $dir = getcwd()."\www\php\menu";
 $files = array_diff(scandir($dir), array(".", ".."));
 foreach ($files as $file){
-  $noextension = explode(".", $file);
-  $name = explode("_", $noextension[0]);
-  echo "<button>".$name[1]."</button>";
+  if (preg_match("#[0-9]{1,2}_[a-zA-Z0-9]+.php#", $file)) {
+    $noextension = explode(".", $file);
+    $name = explode("_", $noextension[0]);
+    echo "<button>".$name[1]."</button>";
+  }
 }
  ?>
