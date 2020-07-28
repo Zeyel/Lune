bb568c7096116e190ee60d201aa79be5b0b8bc8c
BRANSTETT Tim <t.branstett@mairie-stavold.int>
2020-07-28 18:20:20 +0200

    Events menu

--- www/menu.php
+++ www/menu.php
@@ -6,7 +6,7 @@ foreach ($files as $file){
   if (preg_match("#[0-9]{1,2}_[a-zA-Z0-9]+.php#", $file)) {
     $noextension = explode(".", $file);
     $name = explode("_", $noextension[0]);
-    echo "<button>".$name[1]."</button>";
+    echo "<button onclick="">".$name[1]."</button>";
   }
 }
  ?>
