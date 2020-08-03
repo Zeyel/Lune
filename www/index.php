d2d1a680df4e8fa4a1ee5f5142aa2e486ee42943
BRANSTETT Tim <t.branstett@mairie-stavold.int>
2020-08-03 16:33:07 +0200

    Menu + Include des fichiers automatisé

--- www/index.php
+++ www/index.php
@@ -3,15 +3,8 @@ session_start();
 
 require_once '../config.php';
 
-include("Exceptions.php")
+include("exception/IException.php");
+include("exception/CustomException.php");
 include("header.php");
 include("menu.php");
-try {
-    isset($include))
-} catch {
-  echo "<script>console.log('Debug not available')"
-}
-foreach($include as $inc)
-  include("php/".$inc);
-}
  ?>
--- www/menu.php
+++ www/menu.php
@@ -1,15 +1,17 @@
 <script src="js/menu.js"></script>
 <?php
 $tmp = "";
-$dir = getcwd()."\php\menu";
+$dir = getcwd()."\menu";
 $files = array_diff(scandir($dir), array(".", ".."));
 foreach ($files as $file){
   if (preg_match("#[0-9]{1,2}_[a-zA-Z0-9]+.php#", $file)) {
     $noextension = explode(".", $file);
     $name = explode("_", $noextension[0]);
-    echo "<button>".$name[1]."</button>";
     $tmp .= $file."~";
   }
 }
-$include = explode("~", $tmp);
+$include = preg_split("#~#", $tmp, NULL, PREG_SPLIT_NO_EMPTY);
+foreach ($include as $inc) {
+  include("menu/".$inc);
+}
  ?>
