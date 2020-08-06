128aebefc6b30cd60069fac2c516c9f2946a62e7
BRANSTETT Tim <t.branstett@mairie-stavold.int>
2020-08-06 17:25:09 +0200

    Bah alors, on vire un echo qu'il fallait pas virer ?

--- www/menu.php
+++ www/menu.php
@@ -8,6 +8,7 @@ foreach ($files as $file){
   if (preg_match("#[0-9]{1,2}_[a-zA-Z0-9]+.php#", $file)) {
     $noextension = explode(".", $file);
     $name = explode("_", $noextension[0]);
+    echo "<button>".$name[1]."</button>";
     $tmp .= $file."~";
   }
 }
